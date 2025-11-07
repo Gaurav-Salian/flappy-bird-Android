6c13a98169a1c9a34e290322ff9a7b708bae49c1
Gaurav salian <gauravsalian11@gmail.com>
2025-11-07 20:32:33 +0530

    update

--- App.js
+++ App.js
@@ -22,6 +22,7 @@
 
 
 import React from 'react';
+import { Text, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HighScoreProvider } from './context/HighScoreContext';
@@ -32,6 +33,15 @@ import SettingsScreen from './screens/SettingsScreen';
 import AboutScreen from './screens/AboutScreen';
 
 const Stack = createStackNavigator();
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.style = {
+  fontFamily: Platform.select({
+    ios: 'Helvetica Neue', // iOS system font
+    android: 'Roboto',     // Android system font
+  }),
+  color: '#222', // optional, gives a consistent look
+};
 
 export default function App() {
   return (
